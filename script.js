62f127a2be4b50abf9b51339f365cfffc7083dc4
johnnyb3gaming <joshuakingsbury9@gmail.com>
2025-05-21 17:51:12 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,34 +1,46 @@
 
-window.addEventListener("DOMContentLoaded", function () {
-    const chartContainer = document.getElementById("chart");
+function loadChart(id) {
+  const container = document.getElementById("chart" + id);
+  container.innerHTML = "";
+  const type = document.getElementById("type" + id).value;
+  const symbol = document.getElementById("symbol" + id).value || "AAPL";
 
-    const chart = LightweightCharts.createChart(chartContainer, {
-        width: chartContainer.clientWidth,
-        height: 600,
-        layout: {
-            background: { color: "#111" },
-            textColor: "#eee",
-        },
-        grid: {
-            vertLines: { color: "#444" },
-            horzLines: { color: "#444" },
-        },
-        priceScale: {
-            borderColor: "#666",
-        },
-        timeScale: {
-            borderColor: "#666",
-        },
-    });
+  const chart = LightweightCharts.createChart(container, {
+    width: container.clientWidth,
+    height: 300,
+    layout: {
+      background: { color: '#000' },
+      textColor: '#FFF'
+    },
+    grid: {
+      vertLines: { color: '#333' },
+      horzLines: { color: '#333' }
+    }
+  });
 
-    const candleSeries = chart.addCandlestickSeries();
+  let series;
+  if (type === "line") {
+    series = chart.addLineSeries();
+  } else if (type === "area") {
+    series = chart.addAreaSeries();
+  } else if (type === "bar") {
+    series = chart.addBarSeries();
+  } else if (type === "heikinashi") {
+    series = chart.addCandlestickSeries();
+  } else {
+    series = chart.addCandlestickSeries();
+  }
 
-    const sampleData = [
-        { time: 1716172800, open: 100, high: 105, low: 95, close: 102 },
-        { time: 1716176400, open: 102, high: 110, low: 101, close: 108 },
-        { time: 1716180000, open: 108, high: 112, low: 106, close: 109 },
-        { time: 1716183600, open: 109, high: 115, low: 107, close: 114 },
-    ];
+  const sampleData = [
+    { time: 1716172800, open: 100, high: 105, low: 95, close: 102 },
+    { time: 1716176400, open: 102, high: 110, low: 101, close: 108 },
+    { time: 1716180000, open: 108, high: 112, low: 106, close: 109 },
+    { time: 1716183600, open: 109, high: 115, low: 107, close: 114 },
+  ];
 
-    candleSeries.setData(sampleData);
-});
+  if (type === "line" || type === "area") {
+    series.setData(sampleData.map(d => ({ time: d.time, value: d.close })));
+  } else {
+    series.setData(sampleData);
+  }
+}
